1d2aad280ea1f61bf19883058c4d24a6e2e6177a
Sander <h150911@stud.hib.no>
2017-11-24 20:38:38 +0100

    comments

--- js/Plane.js
+++ js/Plane.js
@@ -3,13 +3,14 @@ class Plane {
         this.obj = obj;
         this.speed = speed;
 
+        //define a circle for the plane to move in
         this.curve = new THREE.EllipseCurve(
             0,0,
             radius,radius
             );
 
         this.index = 0;
-        this.roti = Math.PI - (Math.PI / 2);
+        this.roti = (Math.PI / 2);//rotation index, start at 90 degrees and rotate towards -90
 
         //this.startCurve.getPoint(0,this.obj.position);
         let newz = this.curve.getPoint(0).y;
@@ -21,15 +22,17 @@ class Plane {
 
     update(delta) {
         this.index += delta / this.speed;
-        this.roti -= (delta /this.speed)*Math.PI*2;
+        this.roti -= (delta /this.speed)*Math.PI*2;//limit rotation so that it takes this.speed amount of seconds to go 180 degrees
 
         this.index = this.index % 1;
 
+        //get next point in curve 
         let newz = this.curve.getPoint(this.index).y + 1500;
         let newx = this.curve.getPoint(this.index).x + 1500;
         this.obj.position.setX(newx);
         this.obj.position.setZ(newz);
 
+        //set rotation of plane
         this.obj.rotation.set(0,this.roti,0);
 
     }
